1e94f3814056c9e4fc66bc244c3a0c8cfb686aea
Darren Bilby <darrenbilby@gmail.com>
2015-04-20 16:46:02 +1000

    Update runlog2timeline.py

--- runlog2timeline.py
+++ runlog2timeline.py
@@ -1,18 +1,18 @@
 # Simple GRR script to run log2timeline on all machines in a GRR instance via GRR Fuse.
-# This runs log2timeline on a specific set of directories on each host, and then uses psort to send
-# the data to ElasticSearch via the timesketch plugin.
+# This runs log2timeline on a specific set of directories on each host, and then uses 
+# psort to send the data to ElasticSearch via the timesketch plugin.
 
-def RunLog2Timeline(client_id, directories, base_dir='/grrfuse/fs/os'):
+def RunLog2Timeline(client_id, directories, base_dir='/grrfuse'):
   import subprocess
   client_id = client_id.Basename()
   hostname = console_utils.ClientIdToHostname(client_id)
   filename = '/tmp/plaso/%s.plaso' % hostname
   for dir_path in directories:
-    path = os.path.join(base_dir, client_id, dir_path.lstrip('/'))
+    path = os.path.join(base_dir, client_id, 'fs/os', dir_path.lstrip('/'))
     if os.path.exists(path):
-      command = "/usr/bin/log2timeline.py --hashers sha256 -p %s %s  > /dev/null 2>&1" % (filename, path)
+      command = "/usr/bin/log2timeline.py --hashers sha256 -p %s \"%s\" > /dev/null 2>&1" % (filename, path)
       print 'Running: ', command
-      subprocess.check_call(command.split())
+      subprocess.check_call(command, shell=True)
   if os.path.exists(filename):
     import_command = "/usr/bin/psort.py -a -o timesketch --name '%s Timeline' %s" % (hostname, filename)
     print 'Running: ', import_command
